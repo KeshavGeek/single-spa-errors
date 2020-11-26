430c6630491aac00a56ee55570bd463498827757
daniloesk <47666332+daniloesk@users.noreply.github.com>
2020-11-26 06:09:50 -0300

    ng new angular10-routing --prefix ng10rt --no-strict --routing --style css

---
+++ angular10-routing/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng10rt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular10-routing';
+}
