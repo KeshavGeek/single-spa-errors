042ad25977654e34990b3ed3ba3692800d3c3736
daniloesk <47666332+daniloesk@users.noreply.github.com>
2020-12-04 09:14:01 -0300

    URL inside component

--- angular11-basic/extra-webpack.config.js
+++ angular11-basic/extra-webpack.config.js
@@ -1,7 +1,14 @@
 const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+const { inspect } = require('util');
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+  console.log(inspect(singleSpaWebpackConfig));
+
+  singleSpaWebpackConfig.module.rules.forEach(rule => {
+    console.log(`==========`)
+    console.log(inspect(rule));
+  });
 
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
